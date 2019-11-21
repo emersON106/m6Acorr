399eb9836ff2f3b3e8bcbdacb4bb700f8d52962d
Yan Huang <prohuangyan@hotmail.com>
2019-11-21 17:35:53 +0800

    delete

--- run.bat
+++
@@ -1,2 +0,0 @@
-::SET ROPTS=--no-save --no-environ --no-init-file --no-restore --no-Rconsole
-R-Portable\App\R-Portable\bin\Rscript.exe %ROPTS% rscript\\m6acorr.r sampleData\\sample.txt human result\\result.txt 1> aodmodel.log 2>&1
